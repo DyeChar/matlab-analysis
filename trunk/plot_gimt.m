b7499e9a3d383fc4f5b7bd2c695bfc55c3700821
jonwilliford <jonwilliford@e0015483-af26-d693-56ad-89e33a2b65b9>
2010-07-07 19:34:56 +0000

    Now performs one-way ANOVA between the different pvals.

--- trunk/plot_gimt.m
+++ trunk/plot_gimt.m
@@ -1,4 +1,4 @@
-function plot_gimt( gimt )
+function [ allerrors pvals ] = plot_gimt( gimt )
 % gimt is the parameter returned by findBestParameterValue.  It is defined
 % as:
 %   gimt{ p, r, d }, where p is the index of the parameter value (for
@@ -18,16 +18,16 @@ besti = 1;
 worsti = 1;
 bestr = 1;
 worstr = 1;
-allerrors = zeros(size(gimt,1)*size(gimt,2),1);
-pvals = zeros(size(gimt,1)*size(gimt,2),1);
+allerrors = zeros(size(gimt,1),size(gimt,2));
+pvals = zeros(size(gimt,1),size(gimt,2),1);
 
 besterr = 1;
 worsterr = 0;
 for i = 1:size(gimt,1)
     for r = 1:size(gimt,2)
         error = mean( ( gimt{i,r,2} - gimt{i,r,1} ) .^ 2 );
-        allerrors((i-1)*size(gimt,2)+r) = error;
-        pvals((i-1)*size(gimt,2)+r) = gimt{i,r,4};
+        allerrors(i,r) = error;
+        pvals(i,r) = gimt{i,r,4};
         if error < besterr
             besterr = error;
             besti = i;
@@ -39,21 +39,20 @@ for i = 1:size(gimt,1)
         end
     end
 end
+figure;
+anova1(allerrors',pvals(:));
+ylim([min(allerrors(:)) 2*median(allerrors(:))])
+title('Errors');
 
 figure;
 range_i = [besti worsti];
 range_r = [bestr worstr];
-subplot( length(range)+2, 1, 1);
-plot(gimt{range(1), 1, 6}, gimt{range(1), 1, 7});
-title('Input Signal');
+subplot( length(range_i)+2, 1, 1);
+plot(gimt{range_i(1), 1, 6}, gimt{range_i(1), 1, 7});
+title('Example Input Signal');
 
-subplot( length(range)+2 , 1, 2);
-%plot([gimt{ :, runnum, 4 }],allerrors);
-boxplot(allerrors,pvals);
-title('Errors');
-
-for i = 1:length(range)
-     subplot( length(range_i)+2 , 1, i+2);
+for i = 1:length(range_i)
+     subplot( length(range_i)+1 , 1, i+1);
      plot( gimt{ range_i(i), range_r(i), 3 }, [ gimt{range_i(i),range_r(i),1}, gimt{range_i(i),range_r(i),2} ] );
      
     error = mean( ( gimt{range_i(i),range_r(i),2} - gimt{range_i(i),range_r(i),1} ) .^ 2 );
